ba1d89bcb6b491d49648c413b565b1d5653dfc17
Lok <lok@pg95.com>
2024-04-12 13:18:10 +0100

    finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,8 +13,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), uploaded )
 
+function uploaded(req, res) {
+  console.log('file uploaded')
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+}
 
+//{"name":"top-right.png","type":"image/png","size":1454}
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
